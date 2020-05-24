272b2f8c72a66854bb6252a7525fd503a9e1acc6
[jiansorge] <[ji@nsorge.com]>
2020-05-24 12:13:03 -0700

    Updates

---
+++ precache-manifest.7106cac8a2ecf8c41e348cc2cfd77a58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8cf39a92bb38516299c",
+    "url": "/JSCRIPT300-week2-assignment/static/css/main.f2d9c6f9.chunk.css"
+  },
+  {
+    "revision": "f8cf39a92bb38516299c",
+    "url": "/JSCRIPT300-week2-assignment/static/js/main.5eb69bb6.chunk.js"
+  },
+  {
+    "revision": "3906f76cc16daf0034fb",
+    "url": "/JSCRIPT300-week2-assignment/static/js/runtime~main.8e535d7a.js"
+  },
+  {
+    "revision": "e85cbf203bf17c506aec",
+    "url": "/JSCRIPT300-week2-assignment/static/js/2.0ebe9ae7.chunk.js"
+  },
+  {
+    "revision": "f4646f79a255e66583f8b76215749091",
+    "url": "/JSCRIPT300-week2-assignment/index.html"
+  }
+];
